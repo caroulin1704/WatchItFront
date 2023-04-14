1698715ade601b37c840bc8c2efeaa8b0f6dd9c4
caroulin1704 <carod.emeraude@gmail.com>
2023-04-14 17:46:17 +0200

    14/04/23 part2

--- src/componants/Signup.js
+++ src/componants/Signup.js
@@ -1,7 +1,9 @@
-import React from "react";
-import "../styles/Signup.css" 
+import React, { useState } from "react";
+import "../styles/Signup.css";
+import { Button } from "react-bootstrap";
+import { useForm } from "react-hook-form";
 
-export default function Signup(){
+export default function Signup() {
     return (
         <div className="container">
             <form>
@@ -10,21 +12,22 @@ export default function Signup(){
                 <div className="ui form">
                     <div className="field">
                         <label>Name :</label><br></br>
-                        <input type="text" name="name" placeholder="Name" />
+                        <input type="text" name="name" placeholder="Name"/>
                     </div>
                     <div className="field">
                         <label>First name :</label><br></br>
-                        <input type="text" name="surname" placeholder="First Name" />
+                        <input type="text" name="surname" placeholder="First Name"/>
                     </div>
                     <div className="field">
                         <label>Username :</label><br></br>
-                        <input type="text" name="pseudo" placeholder="Username" />
+                        <input type="text" name="pseudo" placeholder="Username"/>
                     </div>
                     <div className="field">
                         <label>Password :</label><br></br>
-                        <input type="text" name="password" placeholder="Password" />
+                        <input type="text" name="password" placeholder="Password"/>
                     </div>
-                    <button className='fluid ui button'>Submit</button>
+                    <br></br>
+                    <Button type="submit" className="btn btn-primary">Sing up !</Button>
                 </div>
             </form>
         </div>
