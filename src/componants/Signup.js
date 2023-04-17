8c3e2aca098eef5a5dc687666519b615e1f32610
caroulin1704 <carod.emeraude@gmail.com>
2023-04-17 14:34:09 +0200

    sign up

--- src/componants/Signup.js
+++ src/componants/Signup.js
@@ -1,35 +1,19 @@
 import React, { useState } from "react";
 import "../styles/Signup.css";
-import { Button } from "react-bootstrap";
-import { useForm } from "react-hook-form";
+import { Card } from "react-bootstrap";
+import Button from "react-bootstrap";
+import Container from "react-bootstrap/Container";
 
 export default function Signup() {
     return (
-        <div className="container">
-            <form>
-                <h1>Join us !</h1>
-                <div className="ui divider"></div>
-                <div className="ui form">
-                    <div className="field">
-                        <label>Name :</label><br></br>
-                        <input type="text" name="name" placeholder="Name"/>
-                    </div>
-                    <div className="field">
-                        <label>First name :</label><br></br>
-                        <input type="text" name="surname" placeholder="First Name"/>
-                    </div>
-                    <div className="field">
-                        <label>Username :</label><br></br>
-                        <input type="text" name="pseudo" placeholder="Username"/>
-                    </div>
-                    <div className="field">
-                        <label>Password :</label><br></br>
-                        <input type="text" name="password" placeholder="Password"/>
-                    </div>
-                    <br></br>
-                    <Button type="submit" className="btn btn-primary">Sing up !</Button>
-                </div>
-            </form>
-        </div>
+        
+        <Container>
+            <Card>
+                <Card.Header id="header">Join Us !</Card.Header>
+                <Card.Body>
+                    
+                </Card.Body>
+            </Card>
+        </Container>
     )
 }
